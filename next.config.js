efb3f6afbca66ec395a51be6718573cdbde5c006
Егор Барашихин <miracles.on.the.turn@gmail.com>
2024-03-06 20:35:45 +0300

    comment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ * This is the configuration object for a Next.js application.
+ */
 const nextConfig = {
+  /**
+   * Enable React's strict mode for additional checks and warnings during development.
+   * @type {boolean}
+   */
   reactStrictMode: true,
-  // Set the basePath based on the environment variable
+
+  // Set the base path for the Next.js application based on the environment variable
+  /**
+   * The base path for the Next.js application.
+   * If the 'BASE_PATH' environment variable is defined, its value will be used;
+   * otherwise, an empty string will be used.
+   * @type {string}
+   */
   basePath: process.env.BASE_PATH || "",
 };
 
