ac8e40c735cf554451bc6b7aacfc2479bfa0fe08
Егор Барашихин <miracles.on.the.turn@gmail.com>
2024-03-06 20:35:44 +0300

    comment

--- components/Skeleton/Skeleton.tsx
+++ components/Skeleton/Skeleton.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import classNames from "classnames";
-import styles from "./Skeleton.module.css";
+import styles from "./Skeleton.module.css"; // Importing the styles from the Skeleton.module.css file
 
 type Props = {
-  width?: string;
-  height?: string;
-  className?: string;
+  width?: string; // Optional width property with string type
+  height?: string; // Optional height property with string type
+  className?: string; // Optional className property with string type
 };
 
+// Skeleton component definition
 export default function Skeleton({ width, height, className }: Props) {
   const skeletonStyles = {
-    width,
-    height,
-    borderRadius: "inherit",
+    width, // Setting the width from the props
+    height, // Setting the height from the props
+    borderRadius: "inherit", // Inheriting the border-radius from the parent element
   };
 
-  const classList = classNames(styles.skeleton, className);
+  const classList = classNames(styles.skeleton, className); // Combining the default and user-defined classes
 
-  return <div style={skeletonStyles} className={classList} />;
+  return <div style={skeletonStyles} className={classList} />; // Returning a div with the combined styles and classNames
 }
+
