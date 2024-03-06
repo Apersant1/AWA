bfa258bd873bf7e4ebec9e44624dee626e824f79
Егор Барашихин <miracles.on.the.turn@gmail.com>
2024-03-06 20:34:35 +0300

    improve

--- components/Skeleton/Skeleton.tsx
+++ components/Skeleton/Skeleton.tsx
@@ -1,20 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 import styles from "./Skeleton.module.css";
 
 type Props = {
   width?: string;
   height?: string;
+  className?: string;
 };
 
-export default function Skeleton({ height, width }: Props) {
-  return (
-    <div
-      style={{
-        width,
-        height,
-        borderRadius: "inherit",
-      }}
-      className={styles.skeleton}
-    />
-  );
+export default function Skeleton({ width, height, className }: Props) {
+  const skeletonStyles = {
+    width,
+    height,
+    borderRadius: "inherit",
+  };
+
+  const classList = classNames(styles.skeleton, className);
+
+  return <div style={skeletonStyles} className={classList} />;
 }
