2af027a16ae93e42b523ffb7bb5d9219ccedb0b9
Егор Барашихин <miracles.on.the.turn@gmail.com>
2024-03-06 20:34:35 +0300

    improve

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "",
+  // Set the basePath based on the environment variable
+  basePath: process.env.BASE_PATH || "",
 };
 
 module.exports = nextConfig;
+
