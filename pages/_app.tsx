0ff0e5096ee13d6798739dc7529798659caf8528
Егор Барашихин <miracles.on.the.turn@gmail.com>
2024-03-06 20:34:36 +0300

    improve

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID ?? ""}
       activeChain={NETWORK}
     >
       {/* Progress bar when navigating between pages */}
@@ -22,8 +22,15 @@ function MyApp({ Component, pageProps }: AppProps) {
 
       {/* Render the navigation menu above each component */}
       <Navbar />
+
       {/* Render the actual component (page) */}
-      <Component {...pageProps} />
+      {!Component ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <Component {...pageProps} />
+        </>
+      )}
     </ThirdwebProvider>
   );
 }
