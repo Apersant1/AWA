f75084bf0de242c09d9cae39b96a77d8834ea8a2
Егор Барашихин <miracles.on.the.turn@gmail.com>
2024-03-06 20:34:42 +0300

    improve

--- pages/sell.tsx
+++ pages/sell.tsx
@@ -1,24 +1,22 @@
-import {
-  ThirdwebNftMedia,
-  useAddress,
-  useContract,
-  useOwnedNFTs,
-} from "@thirdweb-dev/react";
 import React, { useState } from "react";
 import Container from "../components/Container/Container";
 import NFTGrid from "../components/NFT/NFTGrid";
+import { NFT as NFTType } from "@thirdweb-dev/sdk";
+import { useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
+import SaleInfo from "../components/SaleInfo/SaleInfo";
 import { NFT_COLLECTION_ADDRESS } from "../const/contractAddresses";
 import tokenPageStyles from "../styles/Token.module.css";
-import { NFT as NFTType } from "@thirdweb-dev/sdk";
-import SaleInfo from "../components/SaleInfo/SaleInfo";
+import ThirdwebNftMedia from "@thirdweb-dev/react/dist/components/nft/ThirdwebNftMedia";
 
-export default function Sell() {
+interface SalePageProps {}
+
+const Sell: React.FC<SalePageProps> = () => {
   // Load all of the NFTs from the NFT Collection
-  const { contract } = useContract(NFT_COLLECTION_ADDRESS);
+  const contract = useContract(NFT_COLLECTION_ADDRESS);
   const address = useAddress();
   const { data, isLoading } = useOwnedNFTs(contract, address);
 
-  const [selectedNft, setSelectedNft] = useState<NFTType>();
+  const [selectedNft, setSelectedNft] = useState<NFTType | undefined>();
 
   return (
     <Container maxWidth="lg">
@@ -29,13 +27,24 @@ export default function Sell() {
           <NFTGrid
             data={data}
             isLoading={isLoading}
-            overrideOnclickBehavior={(nft) => {
+            overrideOnclickBehavior={(nft: NFTType) => {
               setSelectedNft(nft);
             }}
             emptyText={
               "Looks like you don't own any NFTs in this collection. Head to the buy page to buy some!"
             }
-          />
+            key={(nft: NFTType) => nft.metadata.id}
+          >
+            {(nft: NFTType) => (
+              <ThirdwebNftMedia
+                metadata={nft.metadata}
+                className={tokenPageStyles.image}
+                alt={nft.metadata.name}
+                height={200}
+                width={200}
+              />
+            )}
+          </NFTGrid>
         </>
       ) : (
         <div className={tokenPageStyles.container} style={{ marginTop: 0 }}>
@@ -44,12 +53,16 @@ export default function Sell() {
               <ThirdwebNftMedia
                 metadata={selectedNft.metadata}
                 className={tokenPageStyles.image}
+                alt={selectedNft.metadata.name}
+                height={200}
+                width={200}
               />
               <button
                 onClick={() => {
                   setSelectedNft(undefined);
                 }}
                 className={tokenPageStyles.crossButton}
+                aria-label="Close"
               >
                 X
               </button>
@@ -59,10 +72,10 @@ export default function Sell() {
           <div className={tokenPageStyles.listingContainer}>
             <p>You&rsquo;re about to list the following item for sale.</p>
             <h1 className={tokenPageStyles.title}>
-              {selectedNft.metadata.name}
+              {selectedNft?.metadata.name}
             </h1>
             <p className={tokenPageStyles.collectionName}>
-              Token ID #{selectedNft.metadata.id}
+              Token ID #{selectedNft?.metadata.id}
             </p>
 
             <div className={tokenPageStyles.pricingContainer}>
@@ -71,6 +84,4 @@ export default function Sell() {
           </div>
         </div>
       )}
-    </Container>
-  );
-}
+
